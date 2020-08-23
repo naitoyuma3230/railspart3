3e33f28b96e6fb9ea60fb2cfa20b21f2168e0217
naitoyuma3230 <naitoyuma@outlook.jp>
2020-08-23 13:24:39 +0000

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # タイトル表記についての関数
+  # 引数にViewの:titleを指定し空ならばbase_titleを、page_titleとbase_titleを返す
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,39 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
-  
-    # 重複する文字を変数として定義
-    def setup
-      @base_title = "Ruby on Rails Tutorial Sample App"
-    end
-  
-  # rails testを実行した際,下に記載されたアクションを全て実行しエラーを教えてくれる
-  # ex) static_pages/home getメソッドをhomeアクションに送信する
-  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    
-    # HTMLの内容を検索<title>の内容に"Home | Ruby on ~"が含まれるかテスト
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response:success
-  end
-
 end
