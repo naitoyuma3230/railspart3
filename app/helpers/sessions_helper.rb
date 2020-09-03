d5c6d3b369965f68318adfe646935d28f0c895a9
naitoyuma3230 <naitoyuma@outlook.jp>
2020-09-03 22:48:55 +0000

    login cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,21 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
   
+  # 現在ログインしているユーザーを返す (いる場合)
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+  
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
   
   def logged_in?
